640ddfb5d46d561675beebf2ff687dcfaaa1e94a
Jyothi <jyothigunde468@gmail.com>
2026-01-15 23:38:46 +0530

    Backend as s3

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,13 @@
 provider "aws" {
   region = "us-east-1"
 }
-
+/*
 resource "aws_s3_bucket" "bucket" {
   bucket = "terraform-github-actions-jyothi-gunde"
-  region = "us-east-1"
+}
+*/
+
+resource "aws_instance" "ec2" {
+  ami = "ami-07ff62358b87c7116"
+  instance_type = "t2.nano"
 }
